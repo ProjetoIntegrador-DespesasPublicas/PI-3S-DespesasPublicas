e4278e32a6b8ad0f81232a0b852ca7ca4732e7d8
Oliveira_Ju <107575747+J0li@users.noreply.github.com>
2026-04-05 13:22:28 -0300

    Create homepage.ts

---
+++ 3s-pi-app/src/pages/homepage.ts
@@ -0,0 +1,9 @@
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { appConfig } from './app.config';
+
+const homepage : ApplicationConfig = {
+
+};
+
+export homepage : mergeApplicationConfig ()
