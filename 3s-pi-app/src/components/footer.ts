b1ad3c716c9899b2edbde675b5a10dc41537346a
Oliveira_Ju <107575747+J0li@users.noreply.github.com>
2026-04-05 13:51:18 -0300

    Create footer.ts

---
+++ 3s-pi-app/src/components/footer.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'footer-comp',
+  standalone: true,
+  template: `<p>© 2026 — Portal de Transparência Pública Orçamentária — Uso acadêmico.Desenvolvido para fins educacionais, com foco na Transparência Pública de Dados Orçamentários.</p>`
+})
+export class FooterComponent {
+}
