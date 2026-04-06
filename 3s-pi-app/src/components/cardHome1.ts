a7a4fdfe7c3e2b6d9fd16c2b96d7591ccfc6592e
Oliveira_Ju <107575747+J0li@users.noreply.github.com>
2026-04-05 21:47:00 -0300

    ADD card home

---
+++ 3s-pi-app/src/components/cardHome1.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'footer-comp',
+  standalone: true,
+  template: `
+    <section class="flex">
+      <div class="contents">
+        <div>
+          <h2>Você sabe o que são despesas publicas?</h2>
+          <p>
+            Despesas públicas são o conjunto
+            de gastos realizados pelo governo para atender as necessidades da sociedade e
+            financiar serviços essenciais, como saúde, educação e infraestrutura.
+          </p>
+        </div>
+        <div>
+          <img src="3s-pi-appsrcassetsimagem1.jpg" />
+        </div>
+      </div>
+    </section>
+  `,
+})
+export class card1Component {}
--- 3s-pi-app/src/pages/homepage.ts
+++ 3s-pi-app/src/pages/homepage.ts
@@ -1,2 +1,8 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { Component, inject } from '@angular/core';
+import { provideRouter, RouterOutlet, RouterLink, RouterLinkActive, ActivatedRoute, withHashLocation } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { FooterComponent } from '../components/footer';
+import { HeaderComponent } from '../components/header';
+
+
