051b7e2f7f4327af84ffd81e6e77f9caacb9b44d
Oliveira_Ju <107575747+J0li@users.noreply.github.com>
2026-05-12 22:33:19 -0300

    add pagina Home

--- 3s-pi-app/src/pages/homepage.ts
+++ 3s-pi-app/src/pages/homepage.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../components/footer';
 import { HeaderComponent } from '../components/header';
-import { CardHomeComponent } from '../components/cardHome2';
+import { CardHomeComponent } from '../components/cardHome';
 @Component({
   selector: 'app-homepage',
   standalone: true,
