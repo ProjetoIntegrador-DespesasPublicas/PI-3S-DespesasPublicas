dac6bc9cdfca03acab05e5fdd0114876d4f56d70
Oliveira_Ju <107575747+J0li@users.noreply.github.com>
2026-05-14 19:30:47 -0300

    fix home

--- 3s-pi-app/src/pages/homepage.ts
+++ 3s-pi-app/src/pages/homepage.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../components/footer';
 import { HeaderComponent } from '../components/header';
 import { CardHomeComponent } from '../components/cardHome';
+
 @Component({
   selector: 'app-homepage',
   standalone: true,
